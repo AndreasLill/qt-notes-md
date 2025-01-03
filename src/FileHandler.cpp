6396ce7aa2644fd46f40e36238ddb243c7718efd
Andreas Lill <dev@andreaslill.com>
2025-01-03 13:41:06 +0100

    Changed FileHandler from class to namespace.

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -3,78 +3,81 @@
 #include <QFile>
 #include <QDir>
 
-void FileHandler::saveFile(const QString &path, const QString &data)
+namespace FileHandler
 {
-    if (path.isEmpty())
-        return;
-    if (data.isEmpty())
-        return;
-
-    QFile file(path);
-    file.open(QIODevice::WriteOnly | QIODevice::Text);
-    file.write(data.toUtf8());
-    file.close();
-}
-
-QString FileHandler::readFile(const QString &path)
-{
-    if (path.isEmpty())
-        return {};
-
-    QFile file(path);
-    QTextStream stream(&file);
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    QString data = stream.readAll();
-    file.close();
-    return data;
-}
-
-QString FileHandler::createFile(const QString &path, const QString &name)
-{
-    if (path.isEmpty())
-        return {};
-    if (name.isEmpty())
-        return {};
-    
-    const QString extension = {".md"};
-    int count = 0;
-
-    while (true)
+    void saveFile(const QString &path, const QString &data)
     {
-        QString fileName = (count > 0) ? QString("%1/%2(%3)%4").arg(path, name, QString::number(count), extension) : QString("%1/%2%3").arg(path, name, extension);
-        QFile file(fileName);
-        if (!file.exists())
-        {
-            file.open(QIODevice::WriteOnly | QIODevice::Text);
-            file.close();
-            return fileName;
-        }
+        if (path.isEmpty())
+            return;
+        if (data.isEmpty())
+            return;
 
-        count++;
+        QFile file(path);
+        file.open(QIODevice::WriteOnly | QIODevice::Text);
+        file.write(data.toUtf8());
+        file.close();
     }
-}
 
-QString FileHandler::createFolder(const QString &path, const QString &name)
-{
-    if (path.isEmpty())
-        return {};
-    if (name.isEmpty())
-        return {};
-    
-    int count = 0;
-
-    while (true)
+    QString readFile(const QString &path)
     {
-        QString fullDirName = (count > 0) ? QString("%1/%2(%3)").arg(path, name, QString::number(count)) : QString("%1/%2").arg(path, name);
-        QDir fullDir(fullDirName);
-        if (!fullDir.exists())
-        {
-            QString folderName = (count > 0) ? QString("%1(%2)").arg(name, QString::number(count)) : name;
-            QDir dir(path);
-            dir.mkdir(folderName);
-            return path + "/" + folderName;
-        }
+        if (path.isEmpty())
+            return {};
 
-        count++;
+        QFile file(path);
+        QTextStream stream(&file);
+        file.open(QIODevice::ReadOnly | QIODevice::Text);
+        QString data = stream.readAll();
+        file.close();
+        return data;
+    }
+
+    QString createFile(const QString &path, const QString &name)
+    {
+        if (path.isEmpty())
+            return {};
+        if (name.isEmpty())
+            return {};
+        
+        const QString extension = {".md"};
+        int count = 0;
+
+        while (true)
+        {
+            QString fileName = (count > 0) ? QString("%1/%2(%3)%4").arg(path, name, QString::number(count), extension) : QString("%1/%2%3").arg(path, name, extension);
+            QFile file(fileName);
+            if (!file.exists())
+            {
+                file.open(QIODevice::WriteOnly | QIODevice::Text);
+                file.close();
+                return fileName;
+            }
+
+            count++;
+        }
+    }
+
+    QString createFolder(const QString &path, const QString &name)
+    {
+        if (path.isEmpty())
+            return {};
+        if (name.isEmpty())
+            return {};
+        
+        int count = 0;
+
+        while (true)
+        {
+            QString fullDirName = (count > 0) ? QString("%1/%2(%3)").arg(path, name, QString::number(count)) : QString("%1/%2").arg(path, name);
+            QDir fullDir(fullDirName);
+            if (!fullDir.exists())
+            {
+                QString folderName = (count > 0) ? QString("%1(%2)").arg(name, QString::number(count)) : name;
+                QDir dir(path);
+                dir.mkdir(folderName);
+                return path + "/" + folderName;
+            }
+
+            count++;
+        }
     }
 }
--- src/FileHandler.h
+++ src/FileHandler.h
@@ -3,15 +3,12 @@
 
 #include <QString>
 
-class FileHandler
+namespace FileHandler
 {
-public:
-    static void saveFile(const QString &path, const QString &data);
-    static QString readFile(const QString &path);
-    static QString createFile(const QString &path, const QString &name);
-    static QString createFolder(const QString &path, const QString &name);
-
-    FileHandler() = delete;
-};
+    void saveFile(const QString &path, const QString &data);
+    QString readFile(const QString &path);
+    QString createFile(const QString &path, const QString &name);
+    QString createFolder(const QString &path, const QString &name);
+}
 
 #endif
